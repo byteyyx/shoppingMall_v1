cac60374730ba594adf41e33997d3343b8ff37a7
yyx <byteyyx@163.com>
2020-11-06 17:36:20 +0800

    项目结构创建完成...

---
+++ gmall-admin-web/src/test/java/com/yyx/gmall/adminweb/GmallAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyx.gmall.adminweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
